bf3b36b5fe143de1fe3c9fa7adee65a35a92c8c6
Artem <Nikitin-Artem>
2022-04-22 03:32:18 +0300

    added configuration files and updated pom dependencies

---
+++ src/main/java/org/example/noxml/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.example.noxml.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage() {
+        return "plain-login";
+    }
+}
